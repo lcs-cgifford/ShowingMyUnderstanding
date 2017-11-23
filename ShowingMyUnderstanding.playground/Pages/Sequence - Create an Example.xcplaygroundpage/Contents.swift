2bd2e6f982c088628314e5ae988324fbffc3b40a
Carson Gifford <cgifford@lcs.on.ca>
2017-11-23 11:27:06 -0500

    images

--- ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
@@ -8,11 +8,11 @@
  You can find a simple image on the Internet and attempt to reproduce it.
  
  Or, you can create your own.
-
+ 
  ## Remember
  
  Commit your work as you make progress on this page.
-
+ 
  ## Note
  
  The following two statements are required to make the playground run. Please do not remove.
@@ -21,10 +21,22 @@ import Cocoa
 import PlaygroundSupport
 
 // Create canvas
-let canvas = Canvas(width: 300, height: 300)
-
-// Replace this comment and add your code below...
-
+let canvas = Canvas(width: 200, height: 200)
+canvas.fillColor = Color.init(hue: 200, saturation: 100, brightness: 100, alpha: 100)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 200, height: 200)
+//creating image.
+for x in stride(from: 0, through: 200, by: 10) {
+    canvas.fillColor = Color(hue: 0, saturation: 100, brightness: x / 2, alpha: 100)
+    canvas.drawRectangle(bottomLeftX: x, bottomLeftY: x, width: x, height: x)
+}
+canvas.fillColor = Color.red
+for w in stride(from: 0, through: 200, by: 50) {
+    canvas.drawEllipse(centreX: w, centreY: 10, width: 10, height: 10)
+}
+canvas.fillColor = Color.red
+for w in stride(from: 0, through: 200, by: 50) {
+    canvas.drawEllipse(centreX: 10, centreY: w, width: 10, height: 10)
+}
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
