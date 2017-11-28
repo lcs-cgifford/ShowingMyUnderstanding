5f359406143305d568f4abddfa39b401b549d41a
Carson Gifford <cgifford@lcs.on.ca>
2017-11-28 14:34:06 -0500

    finish

--- ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
@@ -18,7 +18,14 @@ import PlaygroundSupport
 
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
-
+let number = random(from: 0, toButNotIncluding: 2)
+if number == 1 {
+    canvas.fillColor = Color.white
+}
+if number == 0 {
+    canvas.fillColor = Color.blue
+}
+canvas.drawEllipse(centreX: 250, centreY: 250, width: 200, height: 200, borderWidth: 10)
 // Replace this command and add your code below...
 
 /*:
--- ShowingMyUnderstanding.playground/Pages/Iteration - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Iteration - Create an Example.xcplaygroundpage/Contents.swift
@@ -21,6 +21,45 @@ let canvas = Canvas(width: 300, height: 300)
 
 // Replace this comment and add your code below...
 
+for x in stride(from: 0, to: 300, by: 50){
+    
+    
+    
+    canvas.drawEllipse(centreX: x, centreY: x, width: 50, height: 50)
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    canvas.drawRectangle(bottomLeftX: x, bottomLeftY: 160, width: 25, height: 8)
+    
+    
+    
+    canvas.drawRectangle(bottomLeftX: x + 25, bottomLeftY: 140, width: 25, height: 8)
+    
+    
+    
+    
+    
+    
+    
+    canvas.drawRectangle(bottomLeftX: x, bottomLeftY: 30, width: 25, height: 8)
+    
+    
+    
+    canvas.drawRectangle(bottomLeftX: x + 25, bottomLeftY: 10, width: 25, height: 8)
+    
+    
+    
+}
+
+// Replace this comment and add your code below...
+
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
